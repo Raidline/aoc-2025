3beb703059f79c8bdf55a630714a348976d040cb
Pedro Fialho <pedro.fialho@ctw.bmwgroup.com>
2025-12-11 11:49:15 +0000

    [-] * completes 7b

--- exercises/ex_7.c
+++ exercises/ex_7.c
@@ -12,60 +12,41 @@ typedef struct node {
   struct node *right;
 } node;
 
-char **alloc_memory_for_debug_state(array_string *result) {
-  char **debug_state = malloc(result->length * sizeof(char *));
+typedef struct point_counter {
+  int line;
+  int col;
+  long counter;
+  char c;
+} point_counter;
+
+point_counter **alloc_memory_for_debug_state(array_string *result) {
+  point_counter **debug_state =
+      malloc(result->length * sizeof(point_counter *));
 
   for (int i = 0; i < result->length; i++) {
-    debug_state[i] = malloc(result->lines[i]->str_len * sizeof(char));
+    point_counter *p =
+        malloc(result->lines[i]->str_len * sizeof(point_counter));
+    p->counter = 0;
+    debug_state[i] = p;
   }
 
   return debug_state;
 }
 
-void free_tree(node *node) {
-  if (node == NULL) {
-    return;
-  }
-
-  if (node->left == NULL && node->right == NULL) {
-    free(node);
-    return;
-  }
-
-  free_tree(node->left);
-  free_tree(node->right);
-}
-
-void print_tree(node *node) {
-  if (node == NULL) {
-    return;
-  }
-
-  printf("line:[%d],col:[%d]\n", node->line, node->col);
-
-  if (node->left != NULL) {
-    printf("left -> line:[%d],col:[%d]\n", node->left->line, node->left->col);
-  }
-
-  if (node->right != NULL) {
-    printf("right -> line:[%d],col:[%d]\n", node->right->line,
-           node->right->col);
-  }
-
-  print_tree(node->left);
-  print_tree(node->right);
-}
-
-void print_debug_state(char **state, int len, int col_len) {
+void print_debug_state(point_counter **state, int len, int col_len) {
   for (int i = 0; i < len; i++) {
     for (int j = 0; j < col_len; j++) {
-      printf("%c", state[i][j]);
+      if (state[i][j].counter >= 1) {
+        printf("%li", state[i][j].counter);
+      } else {
+        printf("%c", state[i][j].c);
+      }
     }
     printf("\n");
   }
 }
 
-void free_debug_state(char **state, int len) {
+void free_debug_state(point_counter **state, int len) {
   for (int i = 0; i < len; i++) {
     free(state[i]);
   }
@@ -85,88 +66,18 @@ bool location_found(line_string *line, int len, int needle_col) {
   return location_found;
 }
 
-int walk(node *curr) {
-  if (curr == NULL) {
-    return 0;
-  }
-
-  if (curr->left == NULL && curr->right == NULL) {
-    return 1;
-  }
-
-  return walk(curr->left) + walk(curr->right);
-}
-
-int count_tree_paths(node *root) {
-  int res = walk(root);
-  // if (root == NULL) {
-  //   return 1;
-  // }
-
-  // printf("line:[%d],col:[%d]\n", root->line, root->col);
-
-  // return count_tree_paths(root->left) + count_tree_paths(root->right);
-
-  return res;
-}
-
-bool find_node(node *node, int line, int col) {
-  if (node == NULL) {
-    return false;
-  }
-
-  // printf("needle:[%d],col:[%d]-", line, col);
-  // printf("looking into node: line:[%d],col:[%d]\n", node->line, node->col);
-
-  if (node->line == line && node->col == col) {
-    return true;
-  }
-
-  if (col < node->col) {
-    return find_node(node->left, line, col);
-  } else {
-    return find_node(node->right, line, col);
-  }
-}
-
-node *create_node(int line, int col) {
-  node *n = malloc(sizeof(node));
-  n->line = line;
-  n->col = col;
-  n->left = NULL;
-  n->right = NULL;
-  return n;
-}
-
-node *insert_into_tree(node *root, int line, int col) {
-  if (root == NULL) {
-    return create_node(line, col);
-  }
-
-  //todo: this is what is wrong
-  if (root->line == line - 1 && col < root->col) {
-    root->left = insert_into_tree(root->left, line, col);
-  } else if (root->line == line - 1 && col >= root->col) {
-    root->right = insert_into_tree(root->right, line, col);
-  }
-
-  return root;
-}
-
 long ex_7(array_string *result) {
 
-  long count = 0;
-
-  // int times_splitted = 0;
-
-  char **debug_state = alloc_memory_for_debug_state(result);
-  node *parent;
+  point_counter **debug_state = alloc_memory_for_debug_state(result);
 
   for (int column = 0; column < result->lines[0]->str_len; column++) {
-    debug_state[0][column] = result->lines[0]->array_ptr[column];
+    point_counter p = {1, column, debug_state[0][column].counter,
+                       result->lines[0]->array_ptr[column]};
+    debug_state[0][column] = p;
     if (result->lines[0]->array_ptr[column] == 'S') {
       result->lines[1]->array_ptr[column] = '|';
-      parent = create_node(1, column);
+      point_counter p = {1, column, debug_state[1][column].counter + 1, '|'};
+      debug_state[1][column] = p;
       break;
     }
   }
@@ -176,48 +87,72 @@ long ex_7(array_string *result) {
 
     for (int column = 0; column < columns->str_len; column++) {
       char ch = columns->array_ptr[column];
-      debug_state[line][column] = ch;
-      // line_string *top_line = result->lines[line - 1];
-      //  bool has_top_beam = location_found(top_line, top_line->str_len,
-      //  column);
-      bool node_found = find_node(parent, line - 1, column);
+      point_counter p = {line, column, debug_state[line][column].counter, ch};
+      debug_state[line][column] = p;
+      line_string *top_line = result->lines[line - 1];
 
-      if (node_found && ch != '^') {
+      bool has_top_beam = location_found(top_line, top_line->str_len, column);
+      if (has_top_beam && ch != '^') {
         columns->array_ptr[column] = '|';
-        debug_state[line][column] = '|';
-        insert_into_tree(parent, line, column);
-      } else if (node_found && ch == '^') {
+        point_counter p = {line, column,
+                           debug_state[line - 1][column].counter +
+                               debug_state[line][column].counter,
+                           ch};
+
+        debug_state[line][column] = p;
+        //  printf("inserting into right:[%d-%d] of:[%d,%d]\n", line, column,
+        //         node->line, node->col);
+      } else if (has_top_beam && ch == '^') {
         int left_col = column - 1;
-        // if (find_node(parent, line, left_col) == NULL) {
-        //  printf("beam splitted left to line:[%d],column:[%d]\n", line,
-        //         left_col);
-        debug_state[line][left_col] = '|';
-        columns->array_ptr[left_col] = '|';
-        insert_into_tree(parent, line, left_col);
-        // insert_into_tree(parent, line, left_col);
-        //}
+
+        bool left_path = location_found(columns, column, left_col);
+
+        if (left_path) {
+          point_counter p = {line, column,
+                             debug_state[line - 1][column].counter +
+                                 debug_state[line][left_col].counter,
+                             ch};
+
+          debug_state[line][left_col] = p;
+        } else {
+          point_counter p = {line, column,
+                             debug_state[line - 1][column].counter, ch};
+          debug_state[line][left_col] = p;
+        }
+
         int right_col = column + 1;
-        // if (find_node(parent, line, right_col) == NULL) {
-        //  printf("beam splitted right to line:[%d],column:[%d]\n", line,
-        //         right_col);
-        debug_state[line][right_col] = '|';
+        bool right_path = location_found(columns, column, right_col);
+
+        if (right_path) {
+          point_counter pr = {line, column,
+                              debug_state[line - 1][column].counter +
+                                  debug_state[line][right_col].counter,
+                              ch};
+          debug_state[line][right_col] = pr;
+        } else {
+          point_counter pr = {line, column,
+                              debug_state[line - 1][column].counter, ch};
+          debug_state[line][right_col] = pr;
+        }
+
+        columns->array_ptr[left_col] = '|';
         columns->array_ptr[right_col] = '|';
-        insert_into_tree(parent, line, right_col);
-        //}
-        // times_splitted++;
+        //times_splitted++;
       }
     }
   }
 
-  print_debug_state(debug_state, result->length, result->lines[0]->str_len);
-  print_tree(parent);
+  // print_debug_state(debug_state, result->length, result->lines[0]->str_len);
 
-  printf("paths count: %d\n", count_tree_paths(parent));
+  point_counter *last_line = debug_state[result->length - 1];
 
-  free_tree(parent);
+  long found_locations = 0;
+  for (int i = 0; i < result->lines[0]->str_len; i++) {
+    // printf("last line values:[%li]\n", last_line[i].counter);
+    found_locations += last_line[i].counter;
+  }
+
   free_debug_state(debug_state, result->length);
 
-  // count = times_splitted;
-
-  return count;
+  return found_locations;
 }
